864b0fb955cc5a8078f7a1adf838d12625a0614f
SYSTEX\2500633 <pinhsunchen@systex.com>
2025-08-01 13:38:31 +0800

    systex0801

--- src/main/java/com/example/demo/controller/BookingController.java
+++ src/main/java/com/example/demo/controller/BookingController.java
@@ -81,6 +81,7 @@ public class BookingController {
 		return ResponseEntity.ok(ApiResponse.success("查詢成功", bookingDtos));
 	}
 	
+	
 	@GetMapping(params = "phone")
 	public ResponseEntity<ApiResponse<List<BookingDto>>> getBookingByPhone(@RequestParam String phone) {
 		List<BookingDto> bookingDtos = bookingService.getBookingsByPhone(phone);
